611b6cff443019e673ddae4a7200409099fb50cc
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2024-03-23 21:58:41 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CLI-Number-Guess-Game01
+This is a simple command line number guessing game with the help of "TYPESCRIPT" , "NODE JS" and "INQUIRER".
---
+++ index.ts
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random()*5+1);
+
+const answer = await inquirer.prompt([
+  {
+    name: "userGuessedNumber",
+    type: "number",
+    message: "please guess the number b/w 1 to 5",
+  },
+]);
+console.log(randomNumber);
+if (answer.userGuessedNumber === randomNumber) {
+  console.log(`Congrats, you guessed right number.`);
+} else {
+  console.log(`you guessed wrong number`);
+}
