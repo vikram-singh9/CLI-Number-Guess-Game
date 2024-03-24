0c7ad02444699d3016b386d67b24471717d2c542
Vikram <163000375+vikram-singh9@users.noreply.github.com>
2024-03-24 16:13:51 +0500

    Updated index.ts
    added chalk for better user experience.

--- index.ts
+++ index.ts
@@ -1,17 +1,19 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
+import chalk from "chalk";
 const randomNumber = Math.floor(Math.random()*5+1);
 
 const answer = await inquirer.prompt([
   {
     name: "userGuessedNumber",
     type: "number",
-    message: "please guess the number b/w 1 to 5",
+     message: chalk.blue("please guess the number b/w 1 to 5"),
   },
 ]);
 console.log(randomNumber);
 if (answer.userGuessedNumber === randomNumber) {
-  console.log(`Congrats, you guessed right number.`);
+  console.log(chalk.green(`Congrats, you guessed right number.`));
 } else {
-  console.log(`you guessed wrong number`);
+  console.log(chalk.red(`You guessed wrong number`));
 }
+
